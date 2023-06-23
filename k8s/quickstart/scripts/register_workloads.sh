2ab362d14b9f9129c69d9a16a2974fa7dbc58905
Sergio Arroutbi <sarroutb@redhat.com>
2023-06-23 11:08:24 +0200

    Fix errors reported by shellcheck
    Signed-off-by: Sergio Arroutbi <sarroutb@redhat.com>

--- k8s/quickstart/scripts/register_workloads.sh
+++ k8s/quickstart/scripts/register_workloads.sh
@@ -20,6 +20,6 @@
 # TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
 # OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 #
-oc exec -n spire spire-server-0 -- /opt/spire/bin/spire-server entry create -spiffeID spiffe://example.org/ns/spire/sa/spire-agent -selector k8s_sat:cluster:${1} -selector k8s_sat:agent_ns:spire -selector k8s_sat:agent_sa:spire-agent -node -socketPath /run/spire/data/api.sock
+oc exec -n spire spire-server-0 -- /opt/spire/bin/spire-server entry create -spiffeID spiffe://example.org/ns/spire/sa/spire-agent -selector "k8s_sat:cluster:${1}" -selector k8s_sat:agent_ns:spire -selector k8s_sat:agent_sa:spire-agent -node -socketPath /run/spire/data/api.sock
 oc exec -n spire spire-server-0 -- /opt/spire/bin/spire-server entry create -spiffeID spiffe://example.org/ns/default/sa/default -parentID spiffe://example.org/ns/spire/sa/spire-agent -selector k8s:ns:default -selector k8s:sa:default -socketPath /run/spire/data/api.sock
 
